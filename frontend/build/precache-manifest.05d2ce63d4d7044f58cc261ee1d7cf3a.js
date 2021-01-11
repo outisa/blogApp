59ef74b64cda9c9d667461290b1edb1048ab69c8
outisa <savolainen.outi@gmail.com>
2021-01-11 20:53:52 +0200

    Add frontend and backend

---
+++ frontend/build/precache-manifest.05d2ce63d4d7044f58cc261ee1d7cf3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef44c3b7dc124c14174f34beefdc1379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b98de9f7e9a7ebd2c77",
+    "url": "/static/css/main.69ff1516.chunk.css"
+  },
+  {
+    "revision": "a93928b4e7293089bfa9",
+    "url": "/static/js/2.53ed3caa.chunk.js"
+  },
+  {
+    "revision": "098d96cd5327dd3fc6707a77e047f86b",
+    "url": "/static/js/2.53ed3caa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b98de9f7e9a7ebd2c77",
+    "url": "/static/js/main.2132b92e.chunk.js"
+  },
+  {
+    "revision": "478e41f145b3e1e7ce88",
+    "url": "/static/js/runtime-main.f2f044e8.js"
+  }
+]);
