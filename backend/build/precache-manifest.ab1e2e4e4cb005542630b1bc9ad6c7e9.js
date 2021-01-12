21f17036bbfdf72a7b0f556ad8fd78039db47386
outisa <savolainen.outi@gmail.com>
2021-01-12 23:16:18 +0200

    Change packake-log.json

---
+++ backend/build/precache-manifest.ab1e2e4e4cb005542630b1bc9ad6c7e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adf4fe9a8daab24bf1b72beea75573c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cb12dbefecec0fbcf50",
+    "url": "/static/css/main.cc3dd0d8.chunk.css"
+  },
+  {
+    "revision": "a7238bb1ddde1b93af57",
+    "url": "/static/js/2.40d95345.chunk.js"
+  },
+  {
+    "revision": "3d66a4ed243bca57a1e68168bbc08249",
+    "url": "/static/js/2.40d95345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb12dbefecec0fbcf50",
+    "url": "/static/js/main.dededb19.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
