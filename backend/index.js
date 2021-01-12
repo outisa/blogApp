e421843e60bf9b65c7ef0a176c253f471815aee4
outisa <savolainen.outi@gmail.com>
2021-01-12 20:54:26 +0200

    Small changes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
-const http = require('http') //eslint-disable-line
+const http = require('http')
 const app = require('./app')
 const config = require('./utils/config')
 
+const server = http.createServer(app)
+
 const PORT = config.PORT
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
